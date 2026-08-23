de57f27cc7f268dc7613fc0ab96a45407e1a9e44
Phil Bogle <philbogle@gmail.com>
2026-08-23 14:50:03 -0700

    ui: move line numbers to their own row above scansion in popup

--- app.js
+++ app.js
@@ -335,8 +335,8 @@
         </div>`;
         data.forEach(l => {
           html += `<div style="padding: 0.5rem 0;">
-            <div class="scansion-capsules full-lines-scansion-capsules" style="justify-content: flex-start; margin-top: 0; padding-bottom: 0;">
-              <div class="full-lines-num">${l.num}.</div>`;
+            <div class="full-lines-num">Line ${l.num}</div>
+            <div class="scansion-capsules full-lines-scansion-capsules" style="justify-content: flex-start; margin-top: 0; padding-bottom: 0;">`;
             
           let half1 = `<div class="scansion-half">`;
           let half2 = `<div class="scansion-half">`;
